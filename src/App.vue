56ce363ea7745d32ac0231adaef426d7c9c9a0bd
Gerardo Vargas <evc414@gmail.com>
2021-05-07 11:10:09 -0500

    show

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="row">
+      <div class="col-md-7">
+        <div class="row">
+          <div class="col-md-6" v-for="prod in productos" :key="prod.id">
+            <producto :producto="prod" v-on:agregar-carro="agregarProdCarro" :estaEnCarrito="estaEnCarrito(prod)"></producto>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md5 my-5">
+        <carrito :items="carrito" v-on:pagar="pagar" v-on:remover-item="removerProducto"></carrito>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import productos from './productos.json'
+import Producto from './components/Producto.vue'
+import Carrito from './components/Carrito.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Producto,
+    Carrito        
+  },
+  data(){
+    return {
+      productos,
+      carrito:[]
+    }
+  },
+  methods:{
+    agregarProdCarro(producto){
+      this.carrito.push(producto);
+    },
+    estaEnCarrito(producto){
+      const item = this.carrito.find(item => item.id === producto.id);
+      if(item){
+        return true;
+      }
+      return false;
+    },
+    removerProducto(producto){
+      this.carrito = this.carrito.filter(item => item.id != producto.id);
+    },
+    pagar(){
+      this.carrito = [];
+      alert('Venta completada !!!');
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
